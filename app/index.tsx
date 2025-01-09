8bd1e7a41fa05da869aadd76935b87c2915ef2e4
pdhruv93 <dhruv.pandey93@gmail.com>
2025-01-09 22:00:19 +0530

    Session creation

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,34 @@
-import * as SecureStore from "expo-secure-store";
-import { LOGGED_IN_USER_EMAIL } from "@/constants";
 import { router } from "expo-router";
+import { supabase } from "@/utils/supabase";
+import * as Linking from "expo-linking";
+import * as QueryParams from "expo-auth-session/build/QueryParams";
+
+const createSessionFromUrl = async (url: string) => {
+  const { params, errorCode } = QueryParams.getQueryParams(url);
+  if (errorCode) throw new Error(errorCode);
+
+  const { token } = params;
+  if (!token) return;
+
+  const { error } = await supabase.auth.verifyOtp({
+    token_hash: token,
+    type: "email",
+  });
+  if (error) throw error;
+};
 
 export default async function App() {
-  let loggedInUserEmailId =
-    await SecureStore.getItemAsync(LOGGED_IN_USER_EMAIL);
+  const url = Linking.useURL();
 
-  if (!loggedInUserEmailId) {
+  if (url) {
+    createSessionFromUrl(url);
+  }
+
+  const { data, error } = await supabase.auth.getSession();
+
+  console.log(":::USer data", data);
+
+  if (error) {
     router.replace("/sign-up");
     return null;
   }
