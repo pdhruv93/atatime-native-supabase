4cffa70661c74f8490fca3b282fb622c28bba57b
pdhruv93 <dhruv.pandey93@gmail.com>
2025-01-12 23:55:10 +0530

    Update useLoggedInUser.ts

--- hooks/useLoggedInUser.ts
+++ hooks/useLoggedInUser.ts
@@ -1,30 +1,57 @@
 import { supabase } from "@/utils/supabase";
-import { Session } from "@supabase/supabase-js";
-import { useEffect, useState } from "react";
+import { type User, type QueryData } from "@supabase/supabase-js";
+import { useCallback, useEffect, useState } from "react";
+import { useShowToast } from "./useShowToast";
 
 export function useLoggedInUser() {
-  const [session, setSession] = useState<Session | null>(null);
+  const { generateToast } = useShowToast();
+  const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
+  const [user, setUser] = useState<QueryData<any> | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    supabase.auth
-      .getSession()
-      .then((data) => {
-        if (data.data.session) {
-          setSession(data.data.session);
-        }
-      })
-      .catch(() => {
-        setSession(null);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+  const getLoggedInUser = useCallback(async () => {
+    const { data, error } = await supabase.auth.getSession();
+    if (error) {
+      generateToast("user-fetch", "error", error.message);
+      setLoggedInUser(null);
+      return;
+    }
+
+    const user = data.session?.user;
+    setLoggedInUser(user ? user : null);
   }, []);
+
+  const getUserProfileData = useCallback(async () => {
+    if (loggedInUser) {
+      const { data: userProfileData, error } = await supabase
+        .from("user_profile")
+        .select()
+        .eq("user_id", loggedInUser.id);
+
+      if (error) {
+        generateToast("user-fetch", "error", error.message);
+        setUser(null);
+        return;
+      }
+
+      setUser(userProfileData);
+    }
+  }, [loggedInUser?.id]);
+
+  useEffect(() => {
+    getLoggedInUser();
+  }, [getLoggedInUser]);
+
+  useEffect(() => {
+    getUserProfileData().finally(() => {
+      setIsLoading(false);
+    });
+  }, [getUserProfileData]);
 
   return {
     isLoading,
-    loggedInUser: session?.user,
+    loggedInUser: user,
+    getUserProfileData,
     signOut: () => supabase.auth.signOut(),
   };
 }
