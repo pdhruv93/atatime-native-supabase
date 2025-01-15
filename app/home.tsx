5cf94e4e1e6f903d8359d0774a239f2afd44400c
pdhruv93 <dhruv.pandey93@gmail.com>
2025-01-15 13:42:33 +0530

    Update home.tsx

--- app/home.tsx
+++ app/home.tsx
@@ -3,17 +3,19 @@ import { VStack } from "@/components/ui/vstack";
 import { UserAvatar } from "@/components/user-profile";
 import { Box } from "@/components/ui/box";
 import { router } from "expo-router";
-import { useAuthStore } from "@/store/AuthStore";
-import { useShallow } from "zustand/react/shallow";
+import { HStack } from "@/components/ui/hstack";
+import { Heading } from "@/components/ui/heading";
 
 export default function Home() {
-  const [signOut] = useAuthStore(useShallow((s) => [s.signOut]));
-
   return (
     <VStack space="4xl" className="h-full bg-white">
-      <Box className="items-end">
-        <UserAvatar onPress={() => router.push("/profile")} />
-      </Box>
+      <HStack className="w-full justify-between items-center">
+        <Heading size="2xl">@@time</Heading>
+
+        <Box className="items-end">
+          <UserAvatar onPress={() => router.push("/profile")} />
+        </Box>
+      </HStack>
 
       <Text>Main body of app</Text>
     </VStack>
