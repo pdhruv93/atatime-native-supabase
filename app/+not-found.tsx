7d031030163046633191e4ecf6f921f2c71f70ac
pdhruv93 <dhruv.pandey93@gmail.com>
2025-01-15 18:08:54 +0530

    Update routes

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,5 +1,26 @@
-import { Text } from "react-native";
+import { Center } from "@/components/ui/center";
+import { Heading } from "@/components/ui/heading";
+import { VStack } from "@/components/ui/vstack";
+import { Button, ButtonText } from "@/components/ui/button";
+import { router } from "expo-router";
 
 export default function NotFoundScreen() {
-  return <Text>This screen does not exists</Text>;
+  return (
+    <Center className="bg-black h-full">
+      <VStack space="4xl" className="w-full h-full justify-center">
+        <VStack space="md">
+          <Heading size="4xl">@@time</Heading>
+          <Heading size="lg">oops! you ended up to a missing route.</Heading>
+        </VStack>
+
+        <Button
+          size="xl"
+          variant="solid"
+          onPress={() => router.replace("/home")}
+        >
+          <ButtonText>Go back home</ButtonText>
+        </Button>
+      </VStack>
+    </Center>
+  );
 }
--- components/activity-creator/formSchema.ts
+++ components/activity-creator/formSchema.ts
@@ -4,7 +4,7 @@ export const formSchema = z.object({
   name: z
     .string()
     .min(3, { message: "Activity name should be atleast 3 chars" }),
-  description: z.string().optional(),
+  description: z.string().max(100).optional(),
 });
 
 export type FormInputs = z.infer<typeof formSchema>;
